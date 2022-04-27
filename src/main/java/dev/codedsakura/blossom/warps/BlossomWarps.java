1889033dfeac9daca536f3af885795b81d8d1f14
codedsakura <sakuracoded@gmail.com>
2022-04-27 11:56:23 +0300

    create & initialize project

---
+++ src/main/java/dev/codedsakura/blossom/warps/BlossomWarps.java
@@ -0,0 +1,24 @@
+package dev.codedsakura.blossom.warps;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import dev.codedsakura.blossom.lib.BlossomConfig;
+import dev.codedsakura.blossom.lib.BlossomLib;
+import dev.codedsakura.blossom.lib.CustomLogger;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.core.Logger;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+class BlossomWarpsConfig {}
+
+public class BlossomWarps implements ModInitializer {
+    static BlossomWarpsConfig CONFIG = BlossomConfig.load(BlossomWarpsConfig.class, "BlossomWarps.json");
+    public static final Logger LOGGER = CustomLogger.createLogger("BlossomWarps");
+
+    @Override
+    public void onInitialize() {
+        BlossomLib.addCommand(literal("warp")
+                .then(argument("warp", StringArgumentType.string())));
+    }
+}
