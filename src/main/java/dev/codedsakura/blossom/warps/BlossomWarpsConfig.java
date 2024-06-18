0e9d6ce7808d363efe78e0eb95f378f10d1409f3
CodedSakura <me@codedsakura.dev>
2024-06-18 23:10:44 +0300

    switch to new, backwards compatible config

--- src/main/java/dev/codedsakura/blossom/warps/BlossomWarpsConfig.java
+++ src/main/java/dev/codedsakura/blossom/warps/BlossomWarpsConfig.java
@@ -1,7 +1,32 @@
 package dev.codedsakura.blossom.warps;
 
+import dev.codedsakura.blossom.lib.config.BlossomConfig;
 import dev.codedsakura.blossom.lib.teleport.TeleportConfig;
 
-public class BlossomWarpsConfig {
-    TeleportConfig teleportation = null;
+public class BlossomWarpsConfig extends BlossomConfig {
+    TeleportConfig teleportation = new TeleportConfig.Builder()
+            .setStandStill(3)
+            .setCooldown(30)
+            .build();
+
+    @Deprecated
+    int standStill = 3;
+    @Deprecated
+    int cooldown = 30;
+
+    @Override
+    protected int getLatestVersion() {
+        return 1;
+    }
+
+    @Override
+    public boolean update() {
+        if (version == null) {
+            teleportation = new TeleportConfig();
+            teleportation.standStill = standStill;
+            teleportation.cooldown = cooldown;
+            return true;
+        }
+        return false;
+    }
 }
